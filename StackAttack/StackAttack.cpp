9ba3272c40e091bc50a42d26156b4103d1000acc
Arthur <Arthur@DESKTOP-UOF36KQ>
2021-06-16 11:04:26 -0300

    Allegro update main

--- StackAttack/StackAttack.cpp
+++ StackAttack/StackAttack.cpp
@@ -1,30 +1,83 @@
 #include <iostream>
-#include <Windows.h>
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_native_dialog.h>
 #include "Space.h"
 
-int main() {
-	al_init();
+enum TECLAS {UP, DOWN, RIGHT, LEFT};
+
+int main()
+{
+	// Game variables
+	const int length_display = 800;
+	const int high_display = 600;
+	bool stop = false;
+	bool teclas[] = { false, false, false, false };
+	ALLEGRO_DISPLAY* display = NULL;
+	ALLEGRO_EVENT_QUEUE* event_queue = NULL;
+	ALLEGRO_BITMAP* backgound = NULL;
+
+	space gamespace;
+
+
+	// Allegro display and initialization
+	if (!al_init())
+	{
+		al_show_native_message_box(NULL, "Error", "Critical Error", "Unable to initialize Allegro",NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return -1;
+	}
+	display = al_create_display(length_display, high_display);
+	if (!display)
+	{
+		al_show_native_message_box(NULL, "Error", "Critical Error", "Unable create display", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+		return -1;
+	}
+
+	// Devices and addons initialization
 	al_install_keyboard();
 	al_init_image_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
+	al_init_primitives_addon();
 
-	int comp, altu;
+	// Event queue and source registers
+	event_queue = al_create_event_queue();
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(event_queue, al_get_display_event_source(display));
 
-	ALLEGRO_BITMAP* bmp = NULL;
-	ALLEGRO_DISPLAY* display = al_create_display(800, 600);
+	while (!stop)
+	{
+		ALLEGRO_EVENT event;
 
-	bmp = al_load_bitmap("objects/images/blocks.bmp");
-	comp = al_get_bitmap_width(bmp);
-	altu = al_get_bitmap_height(bmp);
+		al_wait_for_event(event_queue, &event);
 
-	al_draw_bitmap(bmp, 800-comp, 600-altu, 0);
-	al_draw_scaled_bitmap(bmp, 25, 75, 25, 25, 0, 0, 50, 50, 0);
-	al_draw_scaled_bitmap(bmp, 25, 75, 25, 25, 50, 0, 50, 50, 0);
+		if (event.type == ALLEGRO_EVENT_KEY_DOWN)
+		{
+			if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+			{
+				stop = true;
+			}
 
-	al_flip_display();
-	al_rest(30.0);
-	al_destroy_bitmap(bmp);
+			// Logical game space
+		}
+
+		else if(event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		{
+			stop = true;
+		}
+
+		// Game flowability
+
+		// Game drawing
+		al_flip_display();
+	}
+
+	// Free memory and game stoping
+	al_destroy_event_queue(event_queue);
+	al_destroy_bitmap(backgound);
 	al_destroy_display(display);
 }
